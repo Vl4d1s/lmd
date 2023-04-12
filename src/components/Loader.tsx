2b0244d300a8f718264969decb7753cbb44e0d8e
Vladis Markin <vladism@wix.com>
2023-04-12 21:56:18 +0300

    change Loader component signature

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,6 @@
-export default function Loader() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <div className="loader ease-linear rounded-full border-t-8 border-gray-200 h-6 w-6 mr-3 animate-spin"></div>
-      <div className="text-gray-700 text-lg font-semibold">{'Loading...'}</div>
-    </div>
-  );
-}
+export const Loader = () => (
+  <div className="fixed inset-0 flex items-center justify-center">
+    <div className="loader ease-linear rounded-full border-t-8 border-gray-200 h-6 w-6 mr-3 animate-spin"></div>
+    <div className="text-gray-700 text-lg font-semibold">Loading...</div>
+  </div>
+);
