830c62ac6b0bf04b6f63c403c267e821ec5efc4e
Vladis Markin <vladism@wix.com>
2023-04-13 20:06:41 +0300

    make the drives page more responsible for mobile

--- src/app/drives/page.tsx
+++ src/app/drives/page.tsx
@@ -16,15 +16,15 @@ export default async function Page() {
     <div className="container mx-auto py-8">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <div className="p-6 flex flex-col sm:flex-row ">
-          <div className="flex items-center mb-4 sm:mb-0">
+          <div className="flex items-center mb-2 sm:mb-0">
             <Image
               alt="Image description"
-              className="mr-2 h-12 w-12"
+              className="mr-2 h-8 w-8 sm:h-12 sm:w-12"
               src={googleDriveLogo}
             />
             <div>
-              <h2 className="text-lg font-semibold">Drives</h2>
-              <p className="text-gray-600">
+              <h2 className="text-base sm:text-lg font-semibold">Drives</h2>
+              <p className="text-gray-600 text-sm sm:text-base">
                 All external and internal drives are listed here.
               </p>
             </div>
