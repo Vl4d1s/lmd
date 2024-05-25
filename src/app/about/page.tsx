94fba067e7df545a40f84d7678276ec599c4f543
Vladis Markin <vladism@earnix.com>
2024-05-25 16:40:04 +0300

    update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ export default function page() {
           <p className="text-lg font-medium my-4">
             Hello there, my name is Vladis and I am proud to be a graduate of
             SCE College where I studied from 2017 to 2021. Currently, I am
-            working as an Infrastructure Software Engineer at Wix.com.
+            working as a Frontend Engineer at Earnix, formerly Ex-Wix.com.
           </p>
           <p className="text-lg font-medium my-4">
             During my first year of studies, I quickly realized that there was
@@ -29,19 +29,26 @@ export default function page() {
             students seeking to enhance their learning experience.
           </p>
           <p className="text-lg font-medium my-4">
-            In addition to my work at Wix, I am also an active member of the
+            In addition to my work at Earnix, I am also an active member of the
             LinkedIn community, where I share my personal and professional
             journey and the many challenges I have overcome. It is my hope that
             by doing so, I can inspire others to pursue their own goals and
             never give up on their dreams.
           </p>
-          <p className="text-lg font-medium my-4">
+          <p className="text-lg font-medium my-4 md:space-x-4 flex flex-col md:flex-row">
             <a
-              className="libutton inline-block py-1 px-4 rounded-full text-white bg-blue-500 font-sans"
+              className="libutton inline-block py-1 px-4 rounded-full text-white md:bg-blue-300 bg-blue-500 hover:bg-blue-500 focus:bg-blue-500 font-sans mb-4 md:mb-0 text-center"
               href="https://www.linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=vladismarkin"
               target="_blank"
             >
               Follow on LinkedIn
+            </a>
+            <a
+              className="libutton inline-block py-1 px-4 rounded-full text-white md:bg-blue-300 bg-blue-500 hover:bg-blue-500 focus:bg-blue-500 font-sans text-center"
+              href="https://vladismarkin.com/"
+              target="_blank"
+            >
+              Visit My Website
             </a>
           </p>
         </div>
